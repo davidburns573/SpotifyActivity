99633372e016495caffde57fe439ad5830688c85
David Burns <57238838+davidburns573@users.noreply.github.com>
2021-08-04 21:34:09 -0500

    Update api_calls.js

--- src/api_calls.js
+++ src/api_calls.js
@@ -1,8 +1,6 @@
 import Cookie from 'react-native-cookie';
 import axios from 'axios';
 const fetch = require('node-fetch');
-// const spDcCookie = "AQBQ6DmlhyOeMBJA3_r7FMaAVgrncouV-puT2iF8fu-PTLGXTh4q53axWiQo3gHiPYoi8O8KwDRnzYWVg6M9hlop58Ln4SBw_fyoyio57Wps"
-// const spKeyCookie = "0c29bed4-a9f5-495d-bfc8-40495a61cb1e"
 let token = "BQCRgyO2wPRnZLAEI-Cs1FeM4IYinumdfwNWDYHdIhMWYxrdj93W3lt-zzju3Mxbh4CQBxvP0mALoD1QOKUhcvd-gzkGIWfJt0LTJt9ve3Gldm_WCwQEmyGZ72BKCbxwY12JiabwBlO6TR1o2G3LgR4U9yUC16O6pQEY2W0orFi-YwZEd-jPHr3cQU7OhZRMes0u_sQY2cHp7pr1bqLMAtPZJ7MvFyr4dzfH1Sdf8DC75sSiLRgJbRrn3ozXeG6v_wQ64qqT2EBVYn5tHqORMiYVRElpxt2WK3-_S6ehPBq0";
 
 export default {
